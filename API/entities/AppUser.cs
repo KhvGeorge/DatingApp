d28f3807a1c00419789ea270fe205b275fb9a2bf
Luka <khvgiorgi@gmail.com>
2022-11-04 14:15:41 +0400

    Users controller initialized

---
+++ API/entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace API.Entities
+{ 
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
+
